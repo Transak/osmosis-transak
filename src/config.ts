bf308a2e71d0e58c8442fdc8f0657db867e2726b
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2022-12-10 11:05:19 +0530

    fix(engcc-561): increase default fee

--- src/config.ts
+++ src/config.ts
@@ -2,26 +2,26 @@ import { Network } from './types';
 
 export const networks: Record<string, Network> = {
   main: {
-    provider: `https://osmosis-mainnet-rpc.allthatnode.com:26657/${process.env.ALLTHATNODEAPIKEY}`,
+    provider: `https://osmosis-mainnet-rpc.allthatnode.com:26657/HCsCEAIRk8lnZ7CgFEaLrie8YqtbNX3b`,
     transactionLink: hash => `https://www.mintscan.io/osmosis/txs/${hash}`,
     walletLink: address => `https://www.mintscan.io/osmosis/account/${address}`,
     networkName: 'osmosis',
     chainId: 'osmosis-1',
     bech32Prefix: 'osmo',
     nativeDenom: 'uosmo',
-    defaultTxFee: 200,
-    defaultGas: 200000,
+    defaultTxFee: 500,
+    defaultGas: 250000,
   },
   testnet: {
-    provider: `https://osmosis-testnet-rpc.allthatnode.com:26657/${process.env.ALLTHATNODEAPIKEY}`,
+    provider: `https://osmosis-testnet-rpc.allthatnode.com:26657/HCsCEAIRk8lnZ7CgFEaLrie8YqtbNX3b`,
     transactionLink: hash => `https://www.mintscan.io/osmosis/txs/${hash}`,
     walletLink: address => `https://www.mintscan.io/osmosis/account/${address}`,
     networkName: 'osmosis',
     chainId: 'osmo-test-4',
     bech32Prefix: 'osmo',
     nativeDenom: 'uosmo',
-    defaultTxFee: 200,
-    defaultGas: 200000,
+    defaultTxFee: 500,
+    defaultGas: 250000,
   },
 };
 
